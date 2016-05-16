27dab73851dbddbabc5cc47196b702974d83d428
Satbir <satbir.sandhu9@gmail.com>
2016-05-15 18:35:13 -0700

    put mixed back in place

---
+++ src/com/entity/Emp.java
@@ -0,0 +1,15 @@
+package com.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Emp {
+	List<String> list = new ArrayList<String>();
+	public void add() {
+		list.add("Satbir");
+		list.add("Michael");
+		list.add("Pappu");
+	}
+	
+ 
+}
